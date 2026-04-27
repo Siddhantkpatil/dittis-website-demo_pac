fbf7e1e1964b2dae8753b8826d424be2d9deb4dc
siddhantkpatil <siddhantkpatil2004@gmail.com>
2026-04-27 12:23:41 +0530

    3 version!

---
+++ pipeline.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('SCM') {
+            steps {
+                // get the latest changes from github repository
+                git 'https://github.com/Siddhantkpatil/dittis-website-demo_pac.git'
+            }
+        }
+
+        stage('build docker image') {
+            steps {
+                sh 'docker image build -t siddhant295/jpac_httpd .'
+            }
+        }
+
+        stage('docker login') {
+            steps {
+                sh 'echo "dckr_pat_hQ4D0TYvEe-Tb4zGR-I9_1ZZCkM"|docker login -u siddhant295 --password-stdin'
+            }
+        }
+
+        stage('push docker image') {
+            steps {
+                sh 'docker image push siddhant295/jpac_httpd'
+            }
+        }
+
+        stage('reload the service') {
+            steps {
+                sh 'docker service update --force --image siddhant295/jpac_httpd jpac'
+            }
+        }
+    }
+}
